4f5a370cf4c742d405586a0750fcef4b7c821a27
AliYeganegi <aliyeganegi2002@gmail.com>
2023-10-04 16:37:04 +0330

    employers can see their trial expire date in dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,12 +1,78 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <h4 class="mt-3">hello {{ auth()->user()->name }}</h4>
+            @if (Auth::check() && auth()->user()->user_type == 'employer')
+            <h7 class="mb-5">your trial will expire on {{ auth()->user()->user_trial }}</h7>
+            @endif
+            <div class="col-md-3">
+                <div class="card-counter primary">
+                    <p class="text-center mt-3 lead">
+                        User profile
+                    </p>
+                    <button class="btn btn-primary float-end">View</button>
+                </div>
+            </div>
 
-<div class="container">
-    <div class="row justify-content-center">
-        {{auth()->user()->name}}
-        {{auth()->user()->email}}
+            <div class="col-md-3">
+                <div class="card-counter danger">
+                    <p class="text-center mt-3 lead">
+                        Post job
+                    </p>
+                    <button class="btn btn-primary float-end">View</button>
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class="card-counter success">
+                    <p class="text-center mt-3 lead">
+                        All jobs
+                    </p>
+                    <button class="btn btn-primary float-end">View</button>
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class="card-counter info">
+                    <p class="text-center mt-3 lead">
+                        Item
+                    </p>
+                    <button class="btn btn-primary float-end">View</button>
+                </div>
+            </div>
+        </div>
     </div>
-</div>
+    <style>
+        .card-counter {
+            box-shadow: 2px 2px 10px #DADADA;
+            margin: 5px;
+            padding: 20px 10px;
+            background-color: #fff;
+            height: 130px;
+            border-radius: 5px;
+            transition: .3s linear all;
+        }
 
+        .card-counter.primary {
+            background-color: #007bff;
+            color: #FFF;
+        }
+
+        .card-counter.danger {
+            background-color: #ef5350;
+            color: #FFF;
+        }
+
+        .card-counter.success {
+            background-color: #66bb6a;
+            color: #FFF;
+        }
+
+        .card-counter.info {
+            background-color: #26c6da;
+            color: #FFF;
+        }
+    </style>
 @endsection
