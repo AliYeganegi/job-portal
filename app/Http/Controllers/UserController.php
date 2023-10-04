a4db2b24acc8768e2f33e2b65c49fab236758834
AliYeganegi <aliyeganegi2002@gmail.com>
2023-10-04 16:13:39 +0330

    added user_trial in employer registeration

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -43,7 +43,8 @@ class UserController extends Controller
             'name' => $request['name'],
             'email' => $request['email'],
             'password' => bcrypt($request['password']),
-            'user_type' => self::JOB_POSTER,    
+            'user_type' => self::JOB_POSTER,  
+            'user_trial' => now()->addWeek(),  
         ]);
 
         return redirect()->route('login')->with('successMessage', 'Your account was successfully created :)');
